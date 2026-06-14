380777ceee445680a10cedd3500f4cf82da32f72
PatrickJS <github@patrickjs.com>
2026-06-14 04:18:00 -0700

    fix: grant release publish permissions

--- pipeline.ts
+++ pipeline.ts
@@ -286,7 +286,7 @@ export default definePipeline({
       },
       github: {
         permissions: {
-          contents: "read",
+          contents: "write",
           packages: "write"
         }
       }
